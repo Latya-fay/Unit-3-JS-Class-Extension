26809f375bb784e7648b74555e8e7a3518a11c25
Latya-fay <ndanumulatya7@gmail.com>
2024-12-16 22:58:36 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Your code here
+// Define the Polygon class
+class Polygon {
+    constructor(sides) {
+        this.sides = sides;
+    }
+
+    get countSides() {
+        return this.sides.length;
+    }
+
+    get perimeter() {
+        return this.sides.reduce((sum, side) => sum + side, 0);
+    }
+}
+
+// Define the Triangle class that inherits from Polygon
+class Triangle extends Polygon {
+    get isValid() {
+        if (this.countSides !== 3) return false;
+        const [a, b, c] = this.sides;
+        return (a + b > c) && (a + c > b) && (b + c > a);
+    }
+}
+
+// Define the Square class that inherits from Polygon
+class Square extends Polygon {
+    get isValid() {
+        if (this.countSides !== 4) return false;
+        return this.sides.every(side => side === this.sides[0]);
+    }
+
+    get area() {
+        return this.isValid ? this.sides[0] ** 2 : undefined;
+    }
+}
